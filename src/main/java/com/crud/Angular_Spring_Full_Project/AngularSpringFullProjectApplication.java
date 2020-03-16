43ad9d5cb7341b2359e4dd3232b0b2cabb859a80
Sujan.Lamichhane <sujann42@gmail.com>
2020-03-16 15:12:19 -0500

    project created

---
+++ src/main/java/com/crud/Angular_Spring_Full_Project/AngularSpringFullProjectApplication.java
@@ -0,0 +1,14 @@
+package com.crud.Angular_Spring_Full_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringFullProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AngularSpringFullProjectApplication.class, args);
+	}
+
+}
